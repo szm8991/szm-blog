d91182542464196fb6fdc23b4de69d2151ba4ed2
zm <68761971+szm8991@users.noreply.github.com>
2022-06-04 10:38:40 +0800

    chore: test ci

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,7 @@ const config = defineConfig({
     ['link', { rel: 'icon', href: 'https://avatars.githubusercontent.com/u/68761971?s=48&v=4' }],
   ],
   description: 'Szm,Szm1998,szm,博客,blog,前端,frontend,javascript,node,git,vue3,vite,读书随笔',
-  base: '/blog/',
+  base: '/szm-blog/',
   themeConfig: {
     logo: 'https://avatars.githubusercontent.com/u/68761971?s=48&v=4',
     nav,
