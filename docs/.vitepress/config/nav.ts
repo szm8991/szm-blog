3e1d2ecaab0a0a95cca3710d4e891bd62d95d9d9
zm <68761971+szm8991@users.noreply.github.com>
2022-06-04 11:29:51 +0800

    chore: test ci

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -1,11 +1,11 @@
 export const nav = [
   {
-    text: '文档',
+    text: '文档🦄',
     activeMatch: '/docs/',
     link: '/docs/hello/hello',
   },
   {
-    text: '文章记录',
+    text: '文章记录🐼',
     activeMatch: '/article/',
     link: '/article/milepost/milepost-2021',
   },
